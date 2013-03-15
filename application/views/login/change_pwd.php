bbed6dff6831f86c98c4ad4891648322679ddb7c
Raj <raj_shekar1234@yahoo.co.in>
2013-03-16 01:05:59 +0530

    - css changes

---
+++ application/views/login/change_pwd.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Bootstrap, from Twitter</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <!-- Le styles -->
+        <link href="<?php echo base_url();?>public/assets/css/bootstrap.css" rel="stylesheet">
+        <style type="text/css">
+            body {
+                padding-top: 60px;
+                /*padding-bottom: 40px;*/
+            }
+            .sidebar-nav {
+                padding: 9px 0;
+            }
+            select{
+                width:50px;
+            }
+            label {display:inline-block;width:140px}
+
+            div.error {
+                color: red;
+                padding-left:140px;
+                padding-bottom:9px;
+            }
+
+            #password div {
+                margin-left: 115px;
+            }
+
+            #password input[type="text"] {
+                height: 36px;
+                width:262px;
+            }
+        </style>
+        <link href="<?php echo base_url();?>public/assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+          <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <!-- Le fav and touch icons -->
+        <link rel="shortcut icon" href="images/favicon.ico">
+        <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+        <script src="<?php echo base_url();?>public/assets/js/jquery.js" type="text/javascript"></script>
+        <script src="<?php echo base_url();?>public/assets/js/validate.js" type="text/javascript"></script>
+       <!-- <script src="<?php //echo base_url();?>public/assets/js/jquery_mobile.js" type="text/javascript"></script>-->
+        <script src="<?php echo base_url();?>public/assets/js/jqGrid-4.3.1/js/i18n/grid.locale-en.js" type="text/javascript" language="javascript"></script>
+        <script src="<?php echo base_url();?>public/assets/js/jqGrid-4.3.1/src/grid.base.js" type="text/javascript" language="javascript"></script>
+        <link rel="stylesheet" href="<?php echo base_url();?>public/assets/js/jquery-ui-1.8.21/css/smoothness/jquery-ui-1.8.21.custom.css" />
+        <link href="<?php echo base_url();?>public/assets/js/jqGrid-4.3.1/src/css/ui.jqgrid.css" rel="stylesheet"  />
+        <script>
+            $(document).ready(function(){
+                $.ajaxSetup({
+                    jsonp: null,
+                    jsonpCallback: null
+                });
+                $('#main_div').css('min-height',$(window).height()-150);
+                var base_url = '<?php echo base_url();?>';
+                var site_url='<?php echo site_url(); ?>';
+
+                $("#resetpwd").validate({
+                    rules: {
+                        old_password: {
+                            required : true
+                        },
+                        new_password: {
+                            required : true
+                        },
+                        conf_password: {
+                            required : true,
+                            equalTo : "#new_password"
+                        },
+                    },
+                    messages: {
+                        old_password: {
+                            required : "Please enter old password"
+                        },
+                        new_password: {
+                            required : "Please enter new password"
+                        },
+                        conf_password: {
+                            required : "Please confirm your password",
+                            equalTo  :  "Did not match with entered password",
+                        },
+                    },
+
+                    submitHandler: function()
+                    {
+                        var data = $('#resetpwd').serialize();
+                        var error_msg;
+                        $.ajax({
+                            type: "POST",
+                            data: data,
+                            url: site_url+"login/ChangePwd",
+                            success: function(data)
+                            {
+                                if(data > 0)
+                                {
+                                    error_msg = '<div class="alert" style="margin-top:23px;"><span class="label label-success"><i class="icon-thumbs-up icon-white"></i>Password has been changed successfully.</span></div>';
+                                }
+                                else
+                                {
+                                    error_msg = '<div class="alert" style="margin-top:23px;"><span class="label label-important"><i class="icon-thumbs-down icon-white"></i>Unable to process...please refresh the screen and try again</span></div>';
+                                }
+                            },
+                            complete : function()
+                            {
+                                $('.err_msg').html(error_msg);
+                            }
+                        });
+                    }
+                });
+
+            });
+            function refreshWindow()
+            {
+                document.location.reload();
+            }
+        </script>
+    </head>
+
+    <body OnResize="refreshWindow()">
+        <?php echo $header;?>
+
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div>&nbsp;</div>
+            </div>
+            <div class="row-fluid" id="main_div">
+                <div class="span12">
+                    <form name="resetpwd" id="resetpwd" method="post" >
+                        <input type="hidden" name="id" value="<?php echo '' ?>">
+                        <div class="modal-header">
+                            <h3>Change Password</h3>
+                        </div>
+                        <div class="modal-body">
+                            <div><label>Old Password :</label><input type="password" id="old_password" name="old_password" autocomplete="off" /></div>
+                            <div><label>New Password :</label><input type="password" id="new_password" name="new_password" autocomplete="off" /></div>
+                            <div><label>Confirm Password :</label><input type="password" id="conf_password" name="conf_password"  autocomplete="off"/></div>
+                            <div><input class="btn" type="submit" name="Change" value="Change"> </div>
+                        </div>
+                    </form>
+                </div><!--/span-->
+            </div><!--/row-->
+
+            <hr>
+
+            <footer>
+                <p>&copy; Company 2012</p>
+            </footer>
+
+        </div><!--/.fluid-container-->
+
+        <!-- Le javascript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="<?php echo base_url();?>public/assets/js/bootstrap-transition.js"></script>
+        <script src="<?php echo base_url();?>public/assets/js/bootstrap-alert.js"></script>
+        <script src="<?php echo base_url();?>public/assets/js/bootstrap-modal.js"></script>
+        <script src="<?php echo base_url();?>public/assets/js/bootstrap-dropdown.js"></script>
+        <script src="<?php echo base_url();?>public/assets/js/bootstrap-scrollspy.js"></script>
+        <script src="<?php echo base_url();?>public/assets/js/bootstrap-tab.js"></script>
+        <script src="<?php echo base_url();?>public/assets/js/bootstrap-tooltip.js"></script>
+        <script src="<?php echo base_url();?>public/assets/js/bootstrap-popover.js"></script>
+        <script src="<?php echo base_url();?>public/assets/js/bootstrap-button.js"></script>
+        <script src="<?php echo base_url();?>public/assets/js/bootstrap-collapse.js"></script>
+        <script src="<?php echo base_url();?>public/assets/js/bootstrap-carousel.js"></script>
+        <script src="<?php echo base_url();?>public/assets/js/bootstrap-typeahead.js"></script>
+    </body>
+</html>
--- application/views/stock/add_stock.php
+++ application/views/stock/add_stock.php
@@ -23,9 +23,9 @@
             .custom_border{
                 border:1px solid #aaaaaa;
             }
-			.error {
-				color: red;
-			}
+            div.error {
+                color: red;
+            }
         </style>
         <link href="<?php echo base_url();?>public/assets/css/bootstrap-responsive.css" rel="stylesheet">
 
@@ -40,181 +40,181 @@
         <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
         <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
         <script src="<?php echo base_url();?>public/assets/js/jquery.js" type="text/javascript"></script>
-		<script src="<?php echo base_url();?>public/assets/js/validate.js" type="text/javascript"></script>
-		<script src="<?php echo base_url();?>public/assets/js/bootbox.js" rel="stylesheet"/></script>
-        <script src="<?php echo base_url();?>public/assets/js/blockui.js" type="text/javascript"></script>
-        <script src="<?php echo base_url();?>public/assets/js/jqGrid-4.3.1/js/i18n/grid.locale-en.js" type="text/javascript" language="javascript"></script>
-        <script src="<?php echo base_url();?>public/assets/js/jqGrid-4.3.1/src/grid.base.js" type="text/javascript" language="javascript"></script>
-        <link rel="stylesheet" href="<?php echo base_url();?>public/assets/js/jquery-ui-1.8.21/css/smoothness/jquery-ui-1.8.21.custom.css" />
-        <link href="<?php echo base_url();?>public/assets/js/jqGrid-4.3.1/src/css/ui.jqgrid.css" rel="stylesheet"  />
-        <script>
-            $(document).ready(function(){
-                $.ajaxSetup({
-                    jsonp: null,
-                    jsonpCallback: null
-                });
-                $('.jsave_stock').click(function(){
-					if( ($('#models_id').val() == '') && ($('#imei_number').val() == '') )
-                    {
-						bootbox.alert("Please Select Models Id<br>Please Select Products");
-                    }
-					else if($('#models_id').val() == '')
-                    {
-						bootbox.alert("Please Select Models Id");
-                    }
-					else if($('#imei_number').val() == '') 
-                    {
-						bootbox.alert("Please Select Products");
-                    }
-                    else
-					{
-						$.ajax({
-							type: "POST",
-							url: '<?php echo site_url();?>stock/saveStock',
-							data: $('#stock_form').serialize(),
-							dataType:'json',
-							beforeSend : function(){
-							},
-							success: function(data){
-								//console.log(data);
-								if(data.error_code != '200')
-								{
-									$('#error_msg').show();
-									$('#msg_body').html(data.error_msg);
-									$.blockUI({ message: ''});
-									//alert(data.error_msg);
-								}
-								//console.log(data);
-							},
-							complete: function(){
-								window.location.href='<?php echo site_url()?>stock';
-							}
-						});
-					}
-					
-                });
-				/*$('.jsave_stock').live('click',function(){
-					$("#stock_form").submit();
-				})*/
-				 $("#stock_form").validate({
-                      rules: {
-                            models_id: {
-                                  required : true
-                            },
-							imei_number: {
-                                  required : true
-                            },
-                        },
-                        messages: {
-                            models_id: {
-									required : "Please Select Model"
-                                    },
-                            imei_number: {
-                                    required : "Please enter Product IMEI"
-                                    },
-                        },
-                        submitHandler: function()
-                        {
-                           $.ajax({
-								type: "POST",
-								url: '<?php echo site_url();?>stock/saveStock',
-								data: $('#stock_form').serialize(),
-								dataType:'json',
-								beforeSend : function(){
-								},
-								success: function(data){
-									console.log(data);
-									if(data.error_code != '200')
-									{
-										$('#error_msg').show();
-										$('#msg_body').html(data.error_msg);
-										$.blockUI({ message: ''});
-										//alert(data.error_msg);
-									}
-									//console.log(data);
-								},
-								complete: function(){
-									window.location.href='<?php echo site_url()?>stock';
-								}
-							});
-                         }
-        		});
-                $('.setheight').css('min-height',$(window).height()-230);
-                $('.imei_number').css('height',$(window).height()-330);
-
-                $('.close').live('click',function(){
-                    $.unblockUI();
-                    $('#error_msg').hide();
-                });
+        <script src="<?php echo base_url();?>public/assets/js/validate.js" type="text/javascript"></script>
+        <script src="<?php echo base_url();?>public/assets/js/bootbox.js" rel="stylesheet"/></script>
+    <script src="<?php echo base_url();?>public/assets/js/blockui.js" type="text/javascript"></script>
+    <script src="<?php echo base_url();?>public/assets/js/jqGrid-4.3.1/js/i18n/grid.locale-en.js" type="text/javascript" language="javascript"></script>
+    <script src="<?php echo base_url();?>public/assets/js/jqGrid-4.3.1/src/grid.base.js" type="text/javascript" language="javascript"></script>
+    <link rel="stylesheet" href="<?php echo base_url();?>public/assets/js/jquery-ui-1.8.21/css/smoothness/jquery-ui-1.8.21.custom.css" />
+    <link href="<?php echo base_url();?>public/assets/js/jqGrid-4.3.1/src/css/ui.jqgrid.css" rel="stylesheet"  />
+    <script>
+        $(document).ready(function(){
+            $.ajaxSetup({
+                jsonp: null,
+                jsonpCallback: null
             });
-        </script>
-    </head>
+            /*$('.jsave_stock').click(function(){
+                                    if( ($('#models_id').val() == '') && ($('#imei_number').val() == '') )
+                {
+                                            bootbox.alert("Please Select Models Id<br>Please Select Products");
+                }
+                                    else if($('#models_id').val() == '')
+                {
+                                            bootbox.alert("Please Select Models Id");
+                }
+                                    else if($('#imei_number').val() == '')
+                {
+                                            bootbox.alert("Please Select Products");
+                }
+                else
+                                    {
+                                            $.ajax({
+                                                    type: "POST",
+                                                    url: '<?php echo site_url();?>stock/saveStock',
+                                                    data: $('#stock_form').serialize(),
+                                                    dataType:'json',
+                                                    beforeSend : function(){
+                                                    },
+                                                    success: function(data){
+                                                            //console.log(data);
+                                                            if(data.error_code != '200')
+                                                            {
+                                                                    $('#error_msg').show();
+                                                                    $('#msg_body').html(data.error_msg);
+                                                                    $.blockUI({ message: ''});
+                                                                    //alert(data.error_msg);
+                                                            }
+                                                            //console.log(data);
+                                                    },
+                                                    complete: function(){
+                                                            window.location.href='<?php echo site_url()?>stock';
+                                                    }
+                                            });
+                                    }
 
-    <body>
-        <?php echo $header;?>
+            });*/
+            $('.jsave_stock').live('click',function(){
+                $("#stock_form").submit();
+            })
+            $("#stock_form").validate({
+                rules: {
+                    models_id: {
+                        required : true
+                    },
+                    imei_number: {
+                        required : true
+                    },
+                },
+                messages: {
+                    models_id: {
+                        required : "Please Select Model"
+                    },
+                    imei_number: {
+                        required : "Please enter Product IMEI"
+                    },
+                },
+                submitHandler: function()
+                {
+                    $.ajax({
+                        type: "POST",
+                        url: '<?php echo site_url();?>stock/saveStock',
+                        data: $('#stock_form').serialize(),
+                        dataType:'json',
+                        beforeSend : function(){
+                        },
+                        success: function(data){
+                            console.log(data);
+                            if(data.error_code != '200')
+                            {
+                                $('#error_msg').show();
+                                $('#msg_body').html(data.error_msg);
+                                $.blockUI({ message: ''});
+                                //alert(data.error_msg);
+                            }
+                            //console.log(data);
+                        },
+                        complete: function(){
+                            window.location.href='<?php echo site_url()?>stock';
+                        }
+                    });
+                }
+            });
+            $('.setheight').css('min-height',$(window).height()-230);
+            $('.imei_number').css('height',$(window).height()-330);
 
-        <div class="container-fluid">
-            <div class="row-fluid ">
-                <div class="span12" id="stock_grid">
-                    <form name="stock_form" id="stock_form" method="post" >
-                        <div class="modal-header">
-                            <h3>New Stock</h3>
-                        </div>
-                        <div class="modal-body setheight">
-                            <div><label>Model:
-                                    <select name="models_id" id="models_id">
-                                        <option value="">Select Model</option>
-                                        <?php
-                                        foreach($models as $model) {
-                                            ?>
-                                        <option value="<?php echo $model->id;?>"><?php echo $model->name;?></option>
-<?php
-                                        }
+            $('.close').live('click',function(){
+                $.unblockUI();
+                $('#error_msg').hide();
+            });
+        });
+    </script>
+</head>
+
+<body>
+    <?php echo $header;?>
+
+    <div class="container-fluid">
+        <div class="row-fluid ">
+            <div class="span12" id="stock_grid">
+                <form name="stock_form" id="stock_form" method="post" >
+                    <div class="modal-header">
+                        <h3>New Stock</h3>
+                    </div>
+                    <div class="modal-body setheight">
+                        <div><label>Model:
+                                <select name="models_id" id="models_id">
+                                    <option value="">Select Model</option>
+                                    <?php
+                                    foreach($models as $model) {
                                         ?>
-                                    </select>
-                                </label>
-                            </div>
-                            <div><label>Products:
-                                    <textarea class="imei_number" name="imei_number" id="imei_number"></textarea>
-                                </label></div>
-                            <div>
-                                <a href="#" class="btn btn-primary jsave_stock">Save changes</a>
-                            </div>
+                                    <option value="<?php echo $model->id;?>"><?php echo $model->name;?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </label>
                         </div>
-                    </form>
-                </div><!--/span-->
-            </div><!--/row-->
+                        <div><label>Products:
+                                <textarea class="imei_number" name="imei_number" id="imei_number"></textarea>
+                            </label></div>
+                        <div>
+                            <a href="#" class="btn btn-primary jsave_stock">Save changes</a>
+                        </div>
+                    </div>
+                </form>
+            </div><!--/span-->
+        </div><!--/row-->
 
-            <hr>
+        <hr>
 
-            <footer>
-                <p>&copy; Company 2012</p>
-            </footer>
+        <footer>
+            <p>&copy; Company 2012</p>
+        </footer>
 
-        </div><!--/.fluid-container-->
+    </div><!--/.fluid-container-->
 
-        <!-- Le javascript
-        ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
-        <script src="<?php echo base_url();?>public/assets/js/bootstrap-transition.js"></script>
-        <script src="<?php echo base_url();?>public/assets/js/bootstrap-alert.js"></script>
-        <script src="<?php echo base_url();?>public/assets/js/bootstrap-modal.js"></script>
-        <script src="<?php echo base_url();?>public/assets/js/bootstrap-dropdown.js"></script>
-        <script src="<?php echo base_url();?>public/assets/js/bootstrap-scrollspy.js"></script>
-        <script src="<?php echo base_url();?>public/assets/js/bootstrap-tab.js"></script>
-        <script src="<?php echo base_url();?>public/assets/js/bootstrap-tooltip.js"></script>
-        <script src="<?php echo base_url();?>public/assets/js/bootstrap-popover.js"></script>
-        <script src="<?php echo base_url();?>public/assets/js/bootstrap-button.js"></script>
-        <script src="<?php echo base_url();?>public/assets/js/bootstrap-collapse.js"></script>
-        <script src="<?php echo base_url();?>public/assets/js/bootstrap-carousel.js"></script>
-        <script src="<?php echo base_url();?>public/assets/js/bootstrap-typeahead.js"></script>
-        <div class="modal hide fade in" id="error_msg"  style="display:none">
-            <div class="modal-header">
-                    <a class="close" data-dismiss="modal">×</a>
-                    <h3>Alert</h3>
-                </div>
-                <div class="modal-body" id="msg_body">
-                </div>
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="<?php echo base_url();?>public/assets/js/bootstrap-transition.js"></script>
+    <script src="<?php echo base_url();?>public/assets/js/bootstrap-alert.js"></script>
+    <script src="<?php echo base_url();?>public/assets/js/bootstrap-modal.js"></script>
+    <script src="<?php echo base_url();?>public/assets/js/bootstrap-dropdown.js"></script>
+    <script src="<?php echo base_url();?>public/assets/js/bootstrap-scrollspy.js"></script>
+    <script src="<?php echo base_url();?>public/assets/js/bootstrap-tab.js"></script>
+    <script src="<?php echo base_url();?>public/assets/js/bootstrap-tooltip.js"></script>
+    <script src="<?php echo base_url();?>public/assets/js/bootstrap-popover.js"></script>
+    <script src="<?php echo base_url();?>public/assets/js/bootstrap-button.js"></script>
+    <script src="<?php echo base_url();?>public/assets/js/bootstrap-collapse.js"></script>
+    <script src="<?php echo base_url();?>public/assets/js/bootstrap-carousel.js"></script>
+    <script src="<?php echo base_url();?>public/assets/js/bootstrap-typeahead.js"></script>
+    <div class="modal hide fade in" id="error_msg"  style="display:none">
+        <div class="modal-header">
+            <a class="close" data-dismiss="modal">×</a>
+            <h3>Alert</h3>
         </div>
-    </body>
+        <div class="modal-body" id="msg_body">
+        </div>
+    </div>
+</body>
 </html>
